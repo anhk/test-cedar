1b49fbb0bcd842231047fe6f6517f9128f560c47
anhk <anhk_@126.com>
2017-03-27 19:31:32 +0800

    update test.lua

--- test.lua
+++ test.lua
@@ -18,18 +18,31 @@ cedar.update(c, "http://www.baidu.com/a", 9989.899)
 cedar.update(c, "http://www.sohu.com/", 9979)
 cedar.update(c, "http://www.sina.com/", 9969)
 
-local ret = cedar.match(c, "http://www.baidu.com/aaaa")
+local function dump(ret)
 
-if not ret then
-    print("return nil")
-end
+    if not ret then
+        print("return nil")
+    end
 
-if type(ret) == "number" then
-    print(ret)
-elseif type(ret) == "table" then
-    for _, v in ipairs(ret) do
-        print(v)
+    if type(ret) == "number" then
+        print(ret)
+    elseif type(ret) == "table" then
+        for _, v in ipairs(ret) do
+            print(v)
+        end
     end
 end
+
+local ret = cedar.match(c, "http://www.baidu.com/aaaa")
+print("match http://www.baidu.com/aaaa")
+dump(ret)
+
+local ret = cedar.match(c, "http://www.sohu.com/384asd?asdkfh")
+print("match http://www.sohu.com/384asd?asdkfh")
+dump(ret)
+
+local ret = cedar.match(c, "http://www.google.com/384asd?asdkfh")
+print("match http://www.google.com/384asd?asdkfh")
+dump(ret)
 
 
